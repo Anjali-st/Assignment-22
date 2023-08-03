ef069d2022786a32c13850e34426a42e6e292958
Anjali-st <anjali90908080@gmail.com>
2023-08-03 15:46:16 +0530

    Assignment - 22

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Assignment_22
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Source source = new Source
+            {
+                Id = 111,
+                Name = "Jack Sparrow",
+                Age = 23
+            };
+
+            Destination destination = new Destination()
+            {
+                Description = "Welcome to IMF...",
+                MissionAcceptance = "Yes",
+                PrevMissionAccomplished = true,
+
+            };
+
+            PropertyMapper.MapProperties(source, destination);
+
+            // Display the values of the properties in the Destination class
+            Console.WriteLine("Destination Properties:");
+            Console.WriteLine($"Id: {destination.Id}");
+            Console.WriteLine($"Name: {destination.Name}");
+            Console.WriteLine($"Description: {destination.Description}");
+            Console.WriteLine($"Mission Acceptance Status: {destination.MissionAcceptance}");
+            Console.WriteLine($"Status of Previous Mission Accomplishment: {destination.PrevMissionAccomplished}");
+            Console.ReadKey();
+        }
+    }
+
+}
+
+
+
---
+++ Source.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Assignment_22
+{
+    public class Source
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
